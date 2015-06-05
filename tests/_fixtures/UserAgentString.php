ceb0bb048f099fa73bd217a12a942b7c0c24cbcf
Илья Гусев <mail@igusev.ru>
2015-06-06 01:31:03 +0300

    added fixtures

---
+++ tests/_fixtures/UserAgentString.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace userAgent\userAgent\Tests;
+
+class UserAgentString
+{
+    /**
+     * @var string
+     */
+    private $browser;
+
+    /**
+     * @var string
+     */
+    private $browserVersion;
+
+    /**
+     * @var string
+     */
+    private $os;
+
+    /**
+     * @var string
+     */
+    private $osVersion;
+
+    /**
+     * @var string
+     */
+    private $device;
+
+    /**
+     * @var string
+     */
+    private $deviceVersion;
+
+    /**
+     * @var string
+     */
+    private $string;
+
+    /**
+     * @var bool
+     */
+    private $is_mobile;
+
+    /**
+     * @return string
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+
+    /**
+     * @param string $browser
+     *
+     * @return $this
+     */
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOs()
+    {
+        return $this->os;
+    }
+
+    /**
+     * @param string $os
+     *
+     * @return $this
+     */
+    public function setOs($os)
+    {
+        $this->os = $os;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getosVersion()
+    {
+        return $this->osVersion;
+    }
+
+    /**
+     * @param string $osVersion
+     *
+     * @return $this
+     */
+    public function setosVersion($osVersion)
+    {
+        $this->osVersion = $osVersion;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getString()
+    {
+        return $this->string;
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return $this
+     */
+    public function setString($string)
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getbrowserVersion()
+    {
+        return $this->browserVersion;
+    }
+
+    /**
+     * @param string $browserVersion
+     *
+     * @return $this
+     */
+    public function setbrowserVersion($browserVersion)
+    {
+        $this->browserVersion = $browserVersion;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDevice()
+    {
+        return $this->device;
+    }
+
+    /**
+     * @param string $device
+     *
+     * @return $this
+     */
+    public function setDevice($device)
+    {
+        $this->device = $device;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeviceVersion()
+    {
+        return $this->deviceVersion;
+    }
+
+    /**
+     * @param string $deviceVersion
+     *
+     * @return $this
+     */
+    public function setDeviceVersion($deviceVersion)
+    {
+        $this->deviceVersion = $deviceVersion;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsMobile()
+    {
+        return $this->is_mobile;
+    }
+
+    /**
+     * @param bool $is_mobile
+     */
+    public function setIsMobile($is_mobile)
+    {
+        $this->is_mobile = $is_mobile;
+    }
+}
