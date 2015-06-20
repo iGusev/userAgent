780cacba03ead96bc3070a6fb44a72df88c963f1
Илья Гусев <mail@igusev.ru>
2015-06-20 18:30:42 +0300

    improvements 4 symbianOS

--- src/Detector/OS/J2ME.php
+++ src/Detector/OS/J2ME.php
@@ -8,6 +8,7 @@ class J2ME extends AbstractOSDetector
     protected static $link = 'http://java.sun.com/javame/';
     protected static $name = 'J2ME';
     protected static $regEx = '/J2ME\/MIDP/i';
+    protected static $excludedRegEx = '/SymbianOS/i';
     protected static $isMobile = true;
 
     public static function detectVersion($userAgentString)
--- src/Detector/OperaMini.php
+++ src/Detector/OperaMini.php
@@ -8,13 +8,13 @@ class OperaMini extends AbstractBrowserDetector
     protected static $link = 'http://www.opera.com/mobile/mini';
     protected static $name = 'Opera Mini';
     protected static $regEx = '/Opera Mini|OPiOS/i';
-    protected static $regExVersion = '/[Mini|OPiOS][\/]([.0-9]+)/i';
+    protected static $regExVersion = '/(Opera\ Mini|OPiOS)[\/]([.0-9]+)/i';
     protected static $isMobile = true;
 
     public static function detectVersion($userAgentString)
     {
         if (preg_match(static::$regExVersion, $userAgentString, $regmatch)) {
-            return $regmatch[1];
+            return $regmatch[2];
         }
 
         return 'unknown';
