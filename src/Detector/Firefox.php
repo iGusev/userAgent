c808c46a00e52b52cec7e3b4288b2043eda9a695
Илья Гусев <mail@igusev.ru>
2015-06-20 19:26:46 +0300

    refactorign

--- src/Detector/Firefox.php
+++ src/Detector/Firefox.php
@@ -8,6 +8,6 @@ class Firefox extends AbstractBrowserDetector
     protected static $link = 'http://www.mozilla.org/';
     protected static $name = 'Firefox';
     protected static $regEx = '/Firefox/i';
-    protected static $excludedRegEx = '/Flock|K-Meleon/i';
+    protected static $excludedRegEx = '/Flock|K-Meleon|Opera/i';
 
 }
--- src/Detector/Opera.php
+++ src/Detector/Opera.php
@@ -10,4 +10,15 @@ class Opera extends AbstractBrowserDetector
     protected static $regEx = '/Opera/i';
     protected static $excludedRegEx = '/Opera Mini|Opera Mobi/i';
 
+
+    public static function detectVersion($userAgentString)
+    {
+        if (preg_match('/Version(\/)([.0-9]+)/i', $userAgentString, $regmatch)
+            || preg_match('/Opera(\ |\/)([.0-9]+)/i', $userAgentString, $regmatch)
+        ) {
+            return $regmatch[2];
+        }
+
+        return 'unknown';
+    }
 }
