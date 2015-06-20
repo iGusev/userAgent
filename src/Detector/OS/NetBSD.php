2b20a7526d87457aa025a9bc3ee02dfe0d606564
Илья Гусев <mail@igusev.ru>
2015-06-20 16:48:29 +0300

    netbsd detect version

--- src/Detector/OS/NetBSD.php
+++ src/Detector/OS/NetBSD.php
@@ -8,4 +8,14 @@ class NetBSD extends AbstractOSDetector
     protected static $link = 'http://www.netbsd.org/';
     protected static $name = 'NetBSD';
     protected static $regEx = '/NetBSD/i';
+    protected static $regExVersion = '/NetBSD[\/|\ ]([.0-9]+)/i';
+
+    public static function detectVersion($userAgentString)
+    {
+        if (preg_match(static::$regExVersion, $userAgentString, $regmatch)) {
+            return $regmatch[1];
+        }
+
+        return 'unknown';
+    }
 }
