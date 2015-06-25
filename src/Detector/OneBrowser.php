52c57b1cea88cb112616162a187f5c6e8032d8fa
Илья Гусев <mail@igusev.ru>
2015-06-25 18:11:46 +0300

    fix onebrowser isMobile param

--- src/Detector/OneBrowser.php
+++ src/Detector/OneBrowser.php
@@ -7,4 +7,5 @@ class OneBrowser extends AbstractBrowserDetector
     protected static $link = 'http://one-browser.com/';
     protected static $name = 'OneBrowser';
     protected static $regEx = '/OneBrowser/i';
+    protected static $isMobile = true;
 }
