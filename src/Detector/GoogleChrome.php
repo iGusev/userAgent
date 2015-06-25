b87b383054e2faa70caab2edf1af0eb872102dbd
Илья Гусев <mail@igusev.ru>
2015-06-25 18:12:22 +0300

    fix false detectcs slimboat as google chrome

--- src/Detector/GoogleChrome.php
+++ src/Detector/GoogleChrome.php
@@ -8,5 +8,5 @@ class GoogleChrome extends AbstractBrowserDetector
     protected static $link = 'http://google.com/chrome/';
     protected static $name = 'Google Chrome';
     protected static $regEx = '/Chrome/i';
-    protected static $excludedRegEx = '/114Browser|115Browser|360se|YaBrowser|MRCHROME|Flock|Vivaldi|ChromePlus|CoolNovo|Chromium|Edge/i';
+    protected static $excludedRegEx = '/114Browser|115Browser|360se|YaBrowser|MRCHROME|Flock|Vivaldi|ChromePlus|CoolNovo|Chromium|Edge|SlimBoat/i';
 }
